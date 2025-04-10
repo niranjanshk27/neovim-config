8144f4a02bb317706ed9a25b91d06042c56a6c1f
niranjanshk <niranjanshk27@gmail.com>
2025-04-10 17:06:41 +0545

    neovim config

---
+++ lua/niranjan/init.lua
@@ -0,0 +1,2 @@
+require("niranjan.remap")
+require("niranjan.set")
---
+++ lua/niranjan/remap.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move highlighted line(s) up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- jump half-page with cursor centered
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+
+-- global replace
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", { noremap = true, silent = true })
+
+
+
+
+
